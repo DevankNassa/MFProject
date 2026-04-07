2e0a72b7fb3d319fe834addfd9067c68da853bb3
Devank <52524140+DevankNassa@users.noreply.github.com>
2026-04-07 22:54:16 +0530

    live changes working, styles added properly.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import './App.css';
+import oldData from './oldData.txt';
 
 function App() {
+  const convertoldDataToJson = () => {
+    console.log(oldData);
+  };
+
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Welcome to MF Project</h1>
         <p>React boilerplate ready to go!</p>
       </header>
+      <body>
+        <div className="button-container">
+          <button onClick={() => convertoldDataToJson()}>
+            <span class="button_top"> Convert old data to JSON </span>
+          </button>
+        </div>
+      </body>
     </div>
   );
 }
