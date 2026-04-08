cf67974d4082ba8b921dc85bba5c5611f30bc511
Devank <52524140+DevankNassa@users.noreply.github.com>
2026-04-08 12:08:16 +0530

    Scheme raw data showing up

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
 import './App.css';
-import oldData from './oldData.txt';
+import {useState,useEffect} from 'react';
+
+function SchemeRawData() {
+  const [content, setContent] = useState('');
+
+  useEffect(() => {
+    // Fetch the file from the public folder
+    fetch(process.env.PUBLIC_URL + '/rawData/SchemeData0704262318SS.csv')
+      .then((response) => response.text())
+      .then((text) => setContent(text))
+      .catch((err) => console.error('Error loading file:', err));
+  }, []);
+
+  return (
+    <><h2>Scheme Raw Data</h2><pre>
+      {content}
+    </pre></>
+  );
+}
 
 function App() {
   const convertoldDataToJson = () => {
-    console.log(oldData);
   };
-
-
+  
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Welcome to MF Project</h1>
-        <p>React boilerplate ready to go!</p>
+        <h1>Mutual Fund</h1>
       </header>
-      <body>
+      <div>
+        <SchemeRawData />
         <div className="button-container">
           <button onClick={() => convertoldDataToJson()}>
-            <span class="button_top"> Convert old data to JSON </span>
+            <span className="button_top"> Convert old data to JSON </span>
           </button>
         </div>
-      </body>
+      </div>
     </div>
   );
 }
